268fbf7f566d6e68c308e05eac6bfbf9821ad01e
Sanjarbek <ismatovsanjarbek@yandex.ru>
2025-02-10 17:23:58 +0500

    updated

--- init.sh
+++ init.sh
@@ -1,4 +1,12 @@
-#!usr/bin/bash
+# This script performs the following tasks to optimize Fedora KDE:
+
+# 1. Installs RPM Fusion repositories (both free and nonfree) for additional software packages.
+# 2. Enables the Fedora Cisco OpenH264 repository for H.264 codec support.
+# 3. Updates the core group of packages.
+# 4. Refreshes and updates firmware using fwupd.
+# 5. Installs multimedia codecs and replaces the free versions of ffmpeg and mesa drivers with their full versions.
+# 6. Updates multimedia packages while excluding the PackageKit-gstreamer-plugin.
+#!/usr/bin/bash
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf config-manager setopt fedora-cisco-openh264.enabled=1
 sudo dnf update @core
@@ -6,8 +14,8 @@ sudo dnf4 group update core
 
 # Firmware updates
 sudo fwupdmgr refresh --force
-sudo fwupdmgr get-devices # Lists devices with available updates.
-sudo fwupdmgr get-updates # Fetches list of available updates.
+sudo fwupdmgr get-devices # Lists devices that have available firmware updates.
+sudo fwupdmgr get-updates # Fetches the list of available firmware updates.
 sudo fwupdmgr update
 
 # Codecs
