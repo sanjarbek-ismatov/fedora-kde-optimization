66467fecc57b38fd27204c97b8d9ba5512947ec2
Sanjarbek <ismatovsanjarbek@yandex.ru>
2025-02-13 10:06:34 +0500

    DNS changes

--- debloat.sh
+++ debloat.sh
@@ -2,11 +2,13 @@ sudo dnf install vlc -y
 sudo dnf remove firewalld kontactinterface libreoffice-data kmahjongg kmines kpat kolourpaint skanpage kdeconnectd krdc krdp krfb neochat firewall-config im-chooser mediawriter kmouth toolbox chrony abrt cups rpcbind xwaylandvideobridge dragon elisa-player akonadi-server switcheroo-control @kde-pim @desktop-accessibility @libreoffice @kde-apps @kde-media 
 sudo dnf autoremove -y
 sudo cp ./configs/timesyncd.conf /etc/systemd/
+sudo cp ./configs/resolved.conf /etc/systemd/
 
 sudo systemctl disable avahi-daemon.service
 sudo systemctl stop avahi-daemon.socket
 sudo systemctl disable ModemManager.service
 sudo systemctl disable smartd.service
+sudo systemctl restart systemd-resolved
 
 sudo rm baloo_file.desktop geoclue-demo-agent.desktop orca-autostart.desktop org.kde.discover.notifier.desktop vboxclient.desktop imsettings-start.desktop vmware-user.desktop spice-vdagent.desktop
 
