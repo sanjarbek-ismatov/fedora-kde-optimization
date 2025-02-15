cf248d3fb3beec883724b1f216a7cb38c91eff4d
Sanjarbek <ismatovsanjarbek@yandex.ru>
2025-02-15 09:53:00 +0500

    added qemu and vbox guest additions to remove

--- debloat.sh
+++ debloat.sh
@@ -1,5 +1,5 @@
 sudo dnf install vlc -y
-sudo dnf remove firewalld kontactinterface libreoffice-data kmahjongg kmines kpat kolourpaint skanpage kdeconnectd krdc krdp krfb neochat firewall-config im-chooser mediawriter kmouth toolbox chrony abrt cups rpcbind xwaylandvideobridge dragon elisa-player akonadi-server switcheroo-control @kde-pim @desktop-accessibility @libreoffice @kde-apps @kde-media 
+sudo dnf remove firewalld kontactinterface libreoffice-data kmahjongg kmines kpat kolourpaint skanpage kdeconnectd krdc krdp krfb neochat firewall-config im-chooser mediawriter kmouth toolbox chrony abrt cups rpcbind xwaylandvideobridge dragon elisa-player akonadi-server switcheroo-control virtualbox-guest-additions qemu-guest-agent @kde-pim @desktop-accessibility @libreoffice @kde-apps @kde-media 
 sudo dnf autoremove -y
 sudo cp ./configs/timesyncd.conf /etc/systemd/
 sudo cp ./configs/resolved.conf /etc/systemd/
