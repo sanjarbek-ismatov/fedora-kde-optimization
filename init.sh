ba7ff6cb644c8e8c9e444d820d445b26a2508f5e
Sanjarbek <ismatovsanjarbek@yandex.ru>
2025-02-15 10:14:00 +0500

    Reminders have been added

--- init.sh
+++ init.sh
@@ -5,8 +5,8 @@ mkdir -p .config/fastfetch
 cp /usr/share/fastfetch/presets/neofetch.jsonc ~/.config/fastfetch/config.jsonc
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 sudo dnf config-manager setopt fedora-cisco-openh264.enabled=1
-#sudo dnf update @core -y
-#sudo dnf4 update @core -y
+#sudo dnf update @core -y # I do not recommend using it if you have not had updates yet
+#sudo dnf4 update @core -y # I do not recommend using it if you have not had updates yet
 sudo dnf makecache
 sudo dnf4 makecache
 pkcon refresh
@@ -18,7 +18,7 @@ sudo fwupdmgr update
 
 # Codecs
 sudo dnf swap ffmpeg-free ffmpeg --allowerasing -y
-#sudo dnf update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
+#sudo dnf update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y # It also updates some parts of the system
 sudo dnf swap mesa-va-drivers mesa-va-drivers-freeworld -y
 sudo dnf swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld -y
 sudo dnf swap mesa-va-drivers.i686 mesa-va-drivers-freeworld.i686 -y
