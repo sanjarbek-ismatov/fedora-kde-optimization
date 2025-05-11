bcd805a8ece2d7d36b04600d98ed538c3560d1a7
sanjarbek-ismatov <ismatovsanjarbek@yandex.ru>
2025-05-11 10:13:12 +0500

    some changes

--- debloat.sh
+++ debloat.sh
@@ -1,18 +1,15 @@
-sudo dnf install vlc -y
-sudo dnf remove firewalld kontactinterface libreoffice-data kmahjongg kmines kpat kolourpaint skanpage kdeconnectd krdc krdp krfb neochat firewall-config im-chooser mediawriter kmouth toolbox chrony abrt cups rpcbind xwaylandvideobridge dragon elisa-player akonadi-server switcheroo-control virtualbox-guest-additions qemu-guest-agent @kde-pim @desktop-accessibility @libreoffice @kde-apps @kde-media 
+sudo dnf install plasma-firewall -y
+sudo dnf remove kontactinterface libreoffice-data kmahjongg kmines kpat kolourpaint skanpage kdeconnectd krdc krdp krfb neochat firewall-config im-chooser mediawriter kmouth toolbox chrony abrt cups rpcbind xwaylandvideobridge dragon elisa-player akonadi-server switcheroo-control virtualbox-guest-additions qemu-guest-agent @kde-pim @desktop-accessibility @libreoffice @kde-apps @kde-media 
 sudo dnf autoremove -y
 sudo cp ./configs/timesyncd.conf /etc/systemd/
-sudo cp ./configs/resolved.conf /etc/systemd/
 
 sudo systemctl disable avahi-daemon.service
 sudo systemctl stop avahi-daemon.socket
 sudo systemctl disable ModemManager.service
 sudo systemctl disable smartd.service
-sudo systemctl restart systemd-resolved
 
-sudo rm baloo_file.desktop geoclue-demo-agent.desktop orca-autostart.desktop org.kde.discover.notifier.desktop vboxclient.desktop imsettings-start.desktop vmware-user.desktop spice-vdagent.desktop
 
-sudo rm /usr/share/polkit-1/rules.d/org.freedesktop.GeoClue2.rules /usr/share/polkit-1/rules.d/org.fedoraproject.FirewallD1.rules
+# sudo rm /usr/share/polkit-1/rules.d/org.freedesktop.GeoClue2.rules /usr/share/polkit-1/rules.d/org.fedoraproject.FirewallD1.rules
 
 sudo systemctl enable --now systemd-timesyncd
 sudo timedatectl set-local-rtc 0
--- init.sh
+++ init.sh
@@ -1,15 +1,28 @@
 #!/usr/bin/bash
+
+# configuring dnf 
 sudo cp ./configs/dnf.conf /etc/dnf/
-sudo dnf install powertop nvtop htop btop rocm-smi fastfetch git gh unzip p7zip p7zip-plugins unrar libva-utils -y
+
+# useful packages
+sudo dnf install powertop nvtop htop btop rocm-smi fastfetch git gh unzip p7zip p7zip-plugins unrar libva-utils fish -y
+
+# changing terminal from bash to fish
+chsh -s /usr/bin/fish
+sudo chsh -s /usr/bin/fish
+
+# fastfetch as neofetch
 mkdir -p .config/fastfetch
 cp /usr/share/fastfetch/presets/neofetch.jsonc ~/.config/fastfetch/config.jsonc
+
+# RPM Fusion
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 sudo dnf config-manager setopt fedora-cisco-openh264.enabled=1
-#sudo dnf update @core -y # I do not recommend using it if you have not had updates yet
-#sudo dnf4 update @core -y # I do not recommend using it if you have not had updates yet
+
+# Caching for PackageKit
 sudo dnf makecache
 sudo dnf4 makecache
 pkcon refresh
+
 # Firmware updates
 sudo fwupdmgr refresh --force
 sudo fwupdmgr get-devices # Lists devices that have available firmware updates.
@@ -18,9 +31,9 @@ sudo fwupdmgr update
 
 # Codecs
 sudo dnf swap ffmpeg-free ffmpeg --allowerasing -y
-#sudo dnf update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y # It also updates some parts of the system
 sudo dnf swap mesa-va-drivers mesa-va-drivers-freeworld -y
 sudo dnf swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld -y
 sudo dnf swap mesa-va-drivers.i686 mesa-va-drivers-freeworld.i686 -y
 sudo dnf swap mesa-vdpau-drivers.i686 mesa-vdpau-drivers-freeworld.i686 -y
+sudo dnf install rpmfusion-\*-appstream-data
 vainfo
