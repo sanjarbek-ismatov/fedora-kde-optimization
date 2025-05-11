f54cb610f17483a4e92c585efeb1fe9492bb2f4f
sanjarbek-ismatov <ismatovsanjarbek@yandex.ru>
2025-05-11 12:42:19 +0500

    TLP-switch

--- power-management.sh
+++ power-management.sh
@@ -3,6 +3,21 @@ sudo dnf remove tuned tuned-ppd -y
 sudo dnf install https://kojipkgs.fedoraproject.org//packages/tlp/1.8.0/1.fc43/noarch/tlp-1.8.0-1.fc43.noarch.rpm https://kojipkgs.fedoraproject.org//packages/tlp/1.8.0/1.fc43/noarch/tlp-rdw-1.8.0-1.fc43.noarch.rpm -y
 sudo systemctl enable tlp
 sudo tlp start
+
+# TLP-switch
+git clone https://github.com/sanjarbek-ismatov/TLP-switch
+cd ./TLP-switch
+cmake -B build
+cmake --build build
+sudo cmake --install build
+
+sudo mkdir -p /usr/share/tlp-switch/configs
+sudo cp configs/* /usr/share/tlp-switch/configs
+
+tlp-switch balanced
+
+echo NOTE! use tlp-switch to change profiles
+
 # sudo grubby --update-kernel=ALL --args="amd_iommu=on"
 # sudo grubby --update-kernel=ALL --args="iommu=pt"
 # sudo grubby --update-kernel=ALL --args="lockdown=integrity"
