812174b530eb0ec52971ae6865f8d556ecfd5ec6
sanjarbek-ismatov <ismatovsanjarbek@yandex.ru>
2025-11-07 21:07:27 +0500

    debloat and power management fixes

--- debloat.sh
+++ debloat.sh
@@ -1,15 +1,13 @@
-sudo dnf install plasma-firewall -y
-sudo dnf remove kontactinterface libreoffice-data kmahjongg kmines kpat kolourpaint skanpage kdeconnectd krdc krdp krfb neochat firewall-config im-chooser mediawriter kmouth toolbox chrony abrt cups rpcbind xwaylandvideobridge dragon elisa-player akonadi-server switcheroo-control virtualbox-guest-additions qemu-guest-agent @kde-pim @desktop-accessibility @libreoffice @kde-apps @kde-media 
+sudo dnf remove kontactinterface libreoffice-data libreoffice-core kmahjongg kmines kpat kolourpaint skanpage kdeconnectd krdc krdp krfb neochat firewall-config im-chooser mediawriter kmouth toolbox chrony abrt cups rpcbind xwaylandvideobridge dragon elisa-player akonadi-server switcheroo-control virtualbox-guest-additions qemu-guest-agent @kde-pim @desktop-accessibility @libreoffice @kde-apps @kde-media @admin-tools firewalld
 sudo dnf autoremove -y
 sudo cp ./configs/timesyncd.conf /etc/systemd/
+sudo rm -rf /usr/share/polkit-1/rules.d/org.fedoraproject.FirewallD1.rules
+sudo rm -rf {baloo_file,geoclue-demo-agent,imsettings-start,org.kde.discover.notifier,spice-vdagent,vmware-user}.desktop
 
 sudo systemctl disable avahi-daemon.service
 sudo systemctl stop avahi-daemon.socket
 sudo systemctl disable ModemManager.service
 sudo systemctl disable smartd.service
-
-
-# sudo rm /usr/share/polkit-1/rules.d/org.freedesktop.GeoClue2.rules /usr/share/polkit-1/rules.d/org.fedoraproject.FirewallD1.rules
 
 sudo systemctl enable --now systemd-timesyncd
 sudo timedatectl set-local-rtc 0
--- power-management.sh
+++ power-management.sh
@@ -1,25 +1,12 @@
 #!/usr/bin/bash
 sudo dnf remove tuned tuned-ppd -y
-sudo dnf install https://kojipkgs.fedoraproject.org//packages/tlp/1.8.0/1.fc43/noarch/tlp-1.8.0-1.fc43.noarch.rpm https://kojipkgs.fedoraproject.org//packages/tlp/1.8.0/1.fc43/noarch/tlp-rdw-1.8.0-1.fc43.noarch.rpm -y
+sudo dnf install tlp tlp-rdw
 sudo systemctl enable tlp
+sudo cp configs/tlp.conf /etc/
 sudo tlp start
 
-# TLP-switch
-git clone https://github.com/sanjarbek-ismatov/TLP-switch
-cd ./TLP-switch
-cmake -B build
-cmake --build build
-sudo cmake --install build
-
-sudo mkdir -p /usr/share/tlp-switch/configs
-sudo cp configs/* /usr/share/tlp-switch/configs
-
-tlp-switch balanced
-
-echo NOTE! use tlp-switch to change profiles
-
+sudo cp configs/grub /etc/default
 # sudo grubby --update-kernel=ALL --args="amd_iommu=on"
 # sudo grubby --update-kernel=ALL --args="iommu=pt"
-# sudo grubby --update-kernel=ALL --args="lockdown=integrity"
 # sudo grubby --update-kernel=ALL --args="selinux=0"
-# sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+sudo grub2-mkconfig -o /boot/grub2/grub.cfg
